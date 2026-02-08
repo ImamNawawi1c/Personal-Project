8e31270fbcb0326e255bdadb59ca7075e8868da9
ImamNawawi1c <imamnwq07@gmail.com>
2026-02-08 22:20:20 +0700

    Update apk

--- App-VirusDetected/scanner.py
+++ App-VirusDetected/scanner.py
@@ -1,12 +1,13 @@
-import hashlib
+import hashlib # mengubah isi data jadi hash/sidik jari digital
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SIGNATURE_FILE = os.path.join(BASE_DIR, "signatures.txt")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #ini mengambil alamat dimana folder scanner berada
+SIGNATURE_FILE = os.path.join(BASE_DIR, "signatures.txt") #gabungkan folder di atas dengan signatures.txt/
+# yg bertujuan supaya program selalu tahu dimana dtabase lokasi virus tersimpan
 
-def get_md5(file_path):
+def get_md5(file_path): #filepath itu untuk menginput alamat file
     hash_md5 = hashlib.md5()
-    with open(file_path, 'rb') as f:
+    with open(file_path, 'rb') as f: # rb(read binary, wajib untuk membuka file apapun) dan as f itu file nya disimpan di variabel f
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
@@ -20,9 +21,15 @@ def scan_file(file_path):
         return "File Not Found"
 
     file_hash = get_md5(file_path)
-    signatures = load_signatures()
+    signatures = load_signatures() # jadi file signatures.txt itu sebagai database virus
 
     if file_hash in signatures:
-        return "⚠️Virus Terdeteksi"
-    return "File Aman ✅"
+        result = "Virus Terdeteksi"
+    else:
+        result = "File Aman "
+    log_result(file_path,result)
+    return result
 
+def log_result(file_path,result):
+    with open("log.txt","a") as log:
+        log.write(f"{file_path} -> {result}\n")
